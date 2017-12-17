a6832d1b75f4c6b671b425b7c2640699bcd27af2
Myzreal <r.skupnik@gmail.com>
2017-12-17 20:33:27 +0100

    Add body to the interpret function

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,5 +1,26 @@
 #include "Interpreter.h"
 
 Command Interpreter::interpret(char symbol) {
-    return SHIFT_RIGHT;
+    Command output;
+
+    switch (symbol) {
+        case '>':
+            return SHIFT_RIGHT;
+        case '<':
+            return SHIFT_LEFT;
+        case '+':
+            return INCREMENT;
+        case '-':
+            return DECREMENT;
+        case '.':
+	    return OUTPUT;
+        case ',':
+	    return INPUT;
+        case '[':
+	    return LOOP_START;
+        case ']':
+	    return LOOP_END;
+        default:
+            return UNKNOWN;
+    }
 }
