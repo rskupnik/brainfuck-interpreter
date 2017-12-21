93cf758fcb630143aadb9f18f0f6c4fe1610cd8b
Myzreal <r.skupnik@gmail.com>
2017-12-21 16:52:39 +0100

    Add basic logic to Brainfuck class (not working)

--- src/Brainfuck.cpp
+++ src/Brainfuck.cpp
@@ -1,9 +1,22 @@
 #include "Brainfuck.h"
+#include <iostream>
+
+using namespace std;
 
 void Brainfuck::execute(char program[]) {
+    Command commands[] = {};
+    commands = translate(program);
+    for (int i = 0; i < sizeof(commands)/sizeof(*commands); i++) {
+	cout << commands[i];
+    }
     return;
 }
 
 Command* Brainfuck::translate(char program[]) {
-    return 0;
+    Command output[sizeof(program)];
+    for (int i = 0; i < sizeof(program); i++) {
+        char c = program[i];
+	output[i] = this->interpreter.interpret(c);
+    }
+    return output;
 }
