919484da84cae73f29210d082eac7ed0603b269d
Myzreal <r.skupnik@gmail.com>
2017-12-22 20:32:01 +0100

    Make use of VM methods inside Brainfuck

--- src/Brainfuck.cpp
+++ src/Brainfuck.cpp
@@ -23,6 +23,29 @@ vector<Command> Brainfuck::translate(const string& program) {
 }
 
 void Brainfuck::executeCommand(const Command& cmd) {
-    cout << cmd << endl;
+    switch (cmd) {
+	case SHIFT_RIGHT:
+	    this->vm.shiftRight();
+	    break;
+	case SHIFT_LEFT:
+	    this->vm.shiftLeft();
+	    break;
+	case INCREMENT:
+	    this->vm.increment();
+	    break;
+	case DECREMENT:
+	    this->vm.decrement();
+	    break;
+	case OUTPUT:
+	    char val = this->vm.readValue();
+	    cout << val;
+	    break;
+	//case INPUT:
+	    //char val = 0;
+	    //val << cin;
+	    //this->vm.loadValue(val);
+	    //break;
+    }
+
     return;
 }
